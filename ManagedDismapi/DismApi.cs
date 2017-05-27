270ae31f6137e156d313632a93f05e7708ff383b
Joshua Cotton <jcotton42@outlook.com>
2017-05-26 21:07:51 -0500

    Exception handling and documentation for existing DismApi methods

--- ManagedDismapi/DismApi.cs
+++ ManagedDismapi/DismApi.cs
@@ -1,6 +1,12 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Security;
 
 namespace ManagedDismapi {
+    /// <summary>
+    /// TODO
+    /// </summary>
     public static class DismApi {
         /// <summary>
         /// Initializes the DISM API for this process.
@@ -8,27 +14,43 @@ namespace ManagedDismapi {
         /// <param name="logLevel">The logging level.</param>
         /// <param name="logPath">Relative or absolute path to a log file. Defaults to %windir%\Logs\DISM\dism.log if <c>null</c> is passed.</param>
         /// <param name="scratchPath">Relative or absolute path to a scratch directory. Defaults to %windir%\Temp if <c>null</c> is passed.</param>
-        /// <exception cref="TODO">TODO</exception>
+        /// <exception cref="InvalidOperationException">This method has already been called without a matching <see cref="Shutdown"/> call.</exception>
+        /// <exception cref="SecurityException">The calling process does not have administrative privileges.</exception>
+        /// <remarks>The calling process must have administrative privileges.</remarks>
+        /// <seealso cref="Shutdown"/>
         public static void Initialize(LogLevel logLevel, string logPath, string scratchPath) {
-            HandleHResult(NativeMethods.DismInitialize(logLevel, logPath, scratchPath));
+            try {
+                NativeMethods.DismInitialize(logLevel, logPath, scratchPath);
+            } catch(COMException e) {
+                HandleHResult(e);
+            }
         }
 
         /// <summary>
         /// Shuts down the DISM API for this process.
         /// </summary>
-        /// <exception cref="TODO">TODO</exception>
+        /// <exception cref="InvalidOperationException">DISM sessions are still open.</exception>
+        /// <seealso cref="Initialize"/>
         public static void Shutdown() {
-            HandleHResult(NativeMethods.DismShutdown());
+            try {
+                NativeMethods.DismShutdown();
+            } catch(COMException e) {
+                HandleHResult(e);
+            }
         }
-        
-        internal static void HandleHResult(int hr) {
-            switch(hr) {
-                //TODO
+
+        internal static void HandleHResult(COMException e) {
+            // TODO: looking at the header, there are errors returned that aren't mentioned in the docs, like DISMAPI_E_SHUTDOWN_IN_PROGRESS
+            switch(e.HResult) {
+                //TODO: case NativeMethods.DISMAPI_E_DISMAPI_ALREADY_INITIALIZED (need to find the value of this, not defined in the header)
+                case NativeMethods.ERROR_ELEVATION_REQUIRED_HR:
+                    throw new SecurityException("The calling process does not have administrative privileges.", e);
+                case NativeMethods.DISMAPI_E_OPEN_SESSION_HANDLES:
+                    throw new InvalidOperationException("Session handles are still open.", e);
                 default:
-                    if(hr < 0) {
-                        throw Marshal.GetExceptionForHR(hr);
-                    }
-                    return;
+                    // rethrows the exception, preserving the original stack trace
+                    ExceptionDispatchInfo.Capture(e).Throw();
+                    break;
             }
         }
     }
