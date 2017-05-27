be332bcd786fd7e8b62a38984bc9cb1d0e4441d8
Joshua Cotton <jcotton42@outlook.com>
2017-05-26 20:08:45 -0500

    Add DismApi.Initialize and skeleton HandleHResult

---
+++ ManagedDismapi/DismApi.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ManagedDismapi {
+    public static class DismApi {
+        /// <summary>
+        /// Initializes the DISM API for this process.
+        /// </summary>
+        /// <param name="logLevel">The logging level.</param>
+        /// <param name="logPath">Relative or absolute path to a log file. Defaults to %windir%\Logs\DISM\dism.log if <c>null</c> is passed.</param>
+        /// <param name="scratchPath">Relative or absolute path to a scratch directory. Defaults to %windir%\Temp if <c>null</c> is passed.</param>
+        /// <exception cref="TODO">TODO</exception>
+        public static void Initialize(LogLevel logLevel, string logPath, string scratchPath) {
+            HandleHResult(NativeMethods.DismInitialize(logLevel, logPath, scratchPath));
+        }
+        
+        internal static void HandleHResult(int hr) {
+            switch(hr) {
+                //TODO
+                default:
+                    if(hr < 0) {
+                        throw Marshal.GetExceptionForHR(hr);
+                    }
+                    return;
+            }
+        }
+    }
+}
