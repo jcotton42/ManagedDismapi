a3a64dcbc57ea64d437eeb98b5ae4978f4cbc7b2
Joshua Cotton <jcotton42@outlook.com>
2017-05-26 23:21:27 -0500

    Add WindowsImage

---
+++ ManagedDismapi/WindowsImage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ManagedDismapi {
+    /// <summary>
+    /// Represents a open Windows image.
+    /// </summary>
+    public sealed class WindowsImage : IDisposable {
+        private readonly uint session;
+        private bool disposed;
+
+        internal WindowsImage(uint session) {
+            this.session = session;
+        }
+
+        /// <summary>
+        /// Closes the image's associated session. You will be unable to perform operations on this image after disposing it.
+        /// </summary>
+        public void Dispose() {
+            if(disposed) {
+                return;
+            }
+
+            try {
+                NativeMethods.DismCloseSession(session);
+            } catch(Exception e) {
+                DismApi.HandleHResult(e);
+            }
+
+            disposed = true;
+
+            GC.SuppressFinalize(this);
+        }
+
+        private void EnsureNotDisposed() {
+            if(disposed) {
+                throw new ObjectDisposedException(GetType().FullName, "This image's session has already been closed.");
+            }
+        }
+
+        ~WindowsImage() {
+            try {
+                NativeMethods.DismCloseSession(session);
+            } catch(Exception e) {
+                DismApi.HandleHResult(e);
+            }
+        }
+    }
+}
