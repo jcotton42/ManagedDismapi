d4cb635c0574c23e1a0532224f2daa8d5fb18ba1
Joshua Cotton <jcotton42@outlook.com>
2017-05-29 17:53:00 -0500

    Add ImageInfo

---
+++ ManagedDismapi/ImageInfo.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using ManagedDismapi.Interop;
+
+namespace ManagedDismapi {
+    /// <summary>
+    /// Metadata for a Windows image.
+    /// </summary>
+    public sealed class ImageInfo {
+        internal ImageInfo() { }
+
+        /// <summary>
+        /// The type of image, such as WIM or VHD.
+        /// </summary>
+        public ImageType ImageType { get; private set; }
+
+        /// <summary>
+        /// The 1-based index of the image in the file.
+        /// </summary>
+        public uint ImageIndex { get; private set; }
+
+        /// <summary>
+        /// The name of the image.
+        /// </summary>
+        public string ImageName { get; private set; }
+
+        /// <summary>
+        /// The description of the image.
+        /// </summary>
+        public string ImageDescription { get; private set; }
+
+        /// <summary>
+        /// The size of the image in bytes.
+        /// </summary>
+        public ulong ImageSize { get; private set; }
+
+        /// <summary>
+        /// The architecture of the image.
+        /// </summary>
+        public ImageArchitecture Architecture { get; private set; }
+
+        /// <summary>
+        /// The name of the product.
+        /// </summary>
+        public string ProductName { get; private set; }
+
+        /// <summary>
+        /// The edition of the product.
+        /// </summary>
+        public string EditionId { get; private set; }
+
+        /// <summary>
+        /// The installation type of the image.
+        /// </summary>
+        public string InstallationType { get; private set; }
+
+        /// <summary>
+        /// The HAL type of the operation system.
+        /// </summary>
+        /// <remarks>Will not be set on generalized images.</remarks>
+        public string HAL { get; private set; }
+
+        /// <summary>
+        /// The product type.
+        /// </summary>
+        public string ProductType { get; private set; }
+
+        /// <summary>
+        /// The product suite.
+        /// </summary>
+        public string ProductSuite { get; private set; }
+
+        /// <summary>
+        /// The version of the operating system in the image.
+        /// </summary>
+        public Version Version { get; private set; }
+
+        /// <summary>
+        /// The service pack build of the operating system.
+        /// </summary>
+        public uint ServicePackBuild { get; private set; }
+
+        /// <summary>
+        /// The service pack level of the operating system.
+        /// </summary>
+        public uint ServicePackLevel { get; private set; }
+
+        /// <summary>
+        /// The bootable status of the image.
+        /// </summary>
+        public ImageBootable Bootable { get; private set; }
+
+        /// <summary>
+        /// The relative path to the Windows directory.
+        /// </summary>
+        public string SystemRoot { get; private set; }
+
+        /// <summary>
+        /// The languages in the image.
+        /// </summary>
+        public string[] Languages { get; private set; }
+
+        /// <summary>
+        /// The default language in the image.
+        /// </summary>
+        public string DefaultLanguage { get; private set; }
+
+        /// <summary>
+        /// Additional information for WIM images.
+        /// </summary>
+        public WIMCustomizedInfo CustomizedInfo { get; private set; }
+
+        internal static ImageInfo FromIntPtr(IntPtr ptr) {
+            var dii = Marshal.PtrToStructure<DismImageInfo>(ptr);
+            var info = new ImageInfo {
+                ImageType = dii.ImageType,
+                ImageIndex = dii.ImageIndex,
+                ImageName = dii.ImageName,
+                ImageDescription = dii.ImageDescription,
+                ImageSize = dii.ImageSize,
+                Architecture = dii.Architecture,
+                ProductName = dii.ProductName,
+                EditionId = dii.EditionId,
+                InstallationType = dii.InstallationType,
+                HAL = dii.HAL,
+                ProductType = dii.ProductType,
+                ProductSuite = dii.ProductSuite,
+                Version = new Version((int)dii.MajorVersion, (int)dii.MinorVersion, (int)dii.Build, (int)dii.SpBuild),
+                ServicePackBuild = dii.SpBuild,
+                ServicePackLevel = dii.SpLevel,
+                Bootable = dii.Bootable,
+                SystemRoot = dii.SystemRoot,
+                Languages = Utils.MarshalArray(dii.Languages, dii.LanguageCount, DismString.FromIntPtr)
+                    .Select(ds => ds.Value).ToArray()
+            };
+
+            info.DefaultLanguage = info.Languages[dii.DefaultLanguageIndex];
+
+            if(dii.CustomizedInfo != IntPtr.Zero) {
+                info.CustomizedInfo = WIMCustomizedInfo.FromIntPtr(dii.CustomizedInfo);
+            }
+
+            return info;
+        }
+    }
+}
---
+++ ManagedDismapi/Interop/DismImageInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ManagedDismapi.Interop {
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode, Pack = 1)]
+    internal sealed class DismImageInfo {
+        internal ImageType ImageType;
+        internal uint ImageIndex;
+        internal string ImageName;
+        internal string ImageDescription;
+        internal ulong ImageSize;
+        internal ImageArchitecture Architecture;
+        internal string ProductName;
+        internal string EditionId;
+        internal string InstallationType;
+        internal string HAL;
+        internal string ProductType;
+        internal string ProductSuite;
+        internal uint MajorVersion;
+        internal uint MinorVersion;
+        internal uint Build;
+        internal uint SpBuild;
+        internal uint SpLevel;
+        internal ImageBootable Bootable;
+        internal string SystemRoot;
+        internal IntPtr Languages;
+        internal uint LanguageCount;
+        internal uint DefaultLanguageIndex;
+        internal IntPtr CustomizedInfo;
+    }
+}
